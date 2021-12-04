08ca1ce95d4e7395c5c90e2e6f0063419bb6f7e2
сергей токарев <sseregatokarew@gmail.com>
2021-12-04 16:32:23 +0700

    Добавьте файлы проекта.

---
+++ REC-85/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace REC_85
+{
+
+    class Program
+    {
+        static void TrianglePrint(int CathetLength)
+        {
+            char Border = '*';
+            for (int i = 0; i < CathetLength; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (i != CathetLength - 1)
+                        Console.Write(j == 0 || j == i ? Border : ' ');
+                    else
+                        break;
+                }
+                Console.WriteLine(i == CathetLength - 1 ? new string(Border, CathetLength) : "");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину катета: ");
+            int CathetLength = int.Parse(Console.ReadLine());
+            TrianglePrint(CathetLength);
+            Console.ReadKey();
+        }
+    }
+}
